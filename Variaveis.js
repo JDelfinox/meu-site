7f32944177e5120bcbc792ddc5d81c370420cc0a
JDelfinox <j.bieldelfis@gmail.com>
2023-07-21 21:18:45 -0300

    Usando JS

---
+++ Variaveis.js
@@ -0,0 +1,15 @@
+//usando var ou nada
+//     identificador
+//         atribuição
+//               valor 
+//possívelerro com undefined
+var nome= "Érico";
+sobrenome= "Costa";
+console.log(nome)
+
+// não permite a execução antes da criação (ERRO)
+let nome2 = "Senac"
+console.log( typeof nome2)
+
+const pi=3.141923
+console.log(typeof pi)
---
+++ operadores.js
@@ -0,0 +1,27 @@
+//Aritméticos
+// + Adição
+// - Subtração
+// * Multiplicação
+// / Divisão
+let numeroUm = 5
+let numeroDois = 6
+let numeroTres = -1
+console.log(numeroDois + numeroTres)
+
+//Relacionais
+// > maior que
+// < menor que
+// != ou !== diferente
+// == ou === igual
+// >= maior ou igual
+// <= menor ou igual
+
+let NUmeroString = '6'
+console.log( numeroDois === NUmeroString)
+
+//Lógicos
+// && E
+// ||OU
+// !NÃO
+
+console.log(!(numeroDois === NUmeroString))
