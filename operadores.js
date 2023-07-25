58a25118ccfd5e97aae951769507488eea34ba83
JDelfinox <j.bieldelfis@gmail.com>
2023-07-24 21:06:01 -0300

    Trabalho

--- operadores.js
+++ operadores.js
@@ -3,6 +3,7 @@
 // - Subtração
 // * Multiplicação
 // / Divisão
+// % modulo (resto)
 let numeroUm = 5
 let numeroDois = 6
 let numeroTres = -1
@@ -25,3 +26,27 @@ console.log( numeroDois === NUmeroString)
 // !NÃO
 
 console.log(!(numeroDois === NUmeroString))
+
+//autoincremento
+let a = 0;
+
+a = a + 1;
+console.log(a);
+
+a++;
+console.log(a);
+
+++a;
+console.log(a);
+
+let b = 10;
+
+console.log(b--);
+
+console.log(--b);
+
+//Aritméticos de Atribuição
+let c = 0;
+c = c + 5;
+console.log()
+c += 5;
