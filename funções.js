8e7a768649e3c38a0d042621a653b21be21ec5db
JDelfinox <j.bieldelfis@gmail.com>
2023-07-24 21:51:34 -0300

    Js

---
+++ funções.js
@@ -0,0 +1,21 @@
+let numeroUm = 15
+let numeroDois = 36
+
+function soma(numero1 = 0, numero2 = 0){
+    const resultado = numero1 + numero2;
+    //console.log(resultado)
+    return resultado
+    //retorno
+
+}
+
+console.log(resultado)
+console.log(soma(1,3))
+console.log(soma(numeroUm, numeroDois))
+
+//  (a² + b²)
+function somaQuadrados(valorA, valorB){
+    //return valorA**2 + valorB**2
+    return Math.pow(valorA)
+}
+console.log(somaQuadrados(numeroUm, numeroDois))
